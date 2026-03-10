9f0e8c266617446c2c935041b0ccce9f2576bfd6
guiAdbens <guilherme@adbensempresarial.com.br>
2026-03-10 09:23:38 -0300

    add reponse in auth for code 401

--- frontend-local/src/services/api.js
+++ frontend-local/src/services/api.js
@@ -3,3 +3,21 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
+
+// interceptors for catch 401
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.log("Usuário não autorizado");
+
+      // remove login salvao
+      localStorage.removeItem("user");
+
+      // redireciona para Login
+      window.location.href = "/";
+    }
+
+    return Promise.reject(error);
+  }
+);
